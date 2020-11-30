c3e664851e7c9bedc2f04b5d335cd7b9a4ab6e43
Christian Simon <csimon1127@gmail.com>
2020-11-30 19:21:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,34 @@
 class Student
-
+  attr_accessor :name, :age, :id
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn] 
+  
+  def self.create_table
+    
+  end 
+  
+  def self.drop_table
+    
+    
+    
+    
+  end
+  
+  def initialize(name, age, id = nil)
+    @name = name
+    @age = age
+    @id = id    
+  end
+  
+  def save 
+    
+  end 
+  
+  def self.create 
+    
+    
+    
+  end 
+  
   
 end
