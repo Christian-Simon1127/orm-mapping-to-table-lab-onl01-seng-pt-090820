539201aa2948f404329ef4c2841fc96862d90923
Christian Simon <csimon1127@gmail.com>
2020-11-30 19:58:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
     
     DB[:conn].execute(sql, self.name, self.grade)
     
-    @id = DB[:conn].execute("SELECT last_insert_rowid FROM")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
   end 
   
   def self.create 
