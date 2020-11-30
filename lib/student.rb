ceefc47357b87a9f6ba8d3deb1b6251badd19366
Christian Simon <csimon1127@gmail.com>
2020-11-30 19:47:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,7 +27,15 @@ class Student
   end
   
   def save 
+    sql = <<-SQL
+    INSERT INTO students (name, grade)
+    VALUES (?,?)
     
+    
+    
+    SQL
+    
+    DB[:conn].execute(sql)
   end 
   
   def self.create 
