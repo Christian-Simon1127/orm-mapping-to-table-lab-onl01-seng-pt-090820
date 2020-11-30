d671d8686c9b198b62f213f7939d5b766b7b63b5
Christian Simon <csimon1127@gmail.com>
2020-11-30 19:37:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,6 @@
 class Student
-  attr_accessor :name, :age, :id
+  attr_accessor :name, :age
+  attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn] 
   
